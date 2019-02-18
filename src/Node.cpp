4b9682b30188c6203ad0add63a9bcf0e1f036d97
Percy Camilo Triveño Aucahuasi <percy@blazingdb.com>
2019-02-18 09:44:30 -0500

    Add basic project structure

---
+++ src/Node.cpp
@@ -0,0 +1,15 @@
+/*
+ * Copyright 2019 BlazingDB, Inc.
+ *     Copyright 2019 Cristhian Alberto Gonzales Castillo <cristhian@blazingdb.com>
+ *     Copyright 2019 Jean Pierre Huaroto <jeanpierre@blazingdb.com>
+ *     Copyright 2019 Percy Camilo Triveño Aucahuasi <percy@blazingdb.com>
+ *     Copyright 2019 Christian Noboa Mardini <christian@blazingdb.com>
+ */
+
+#include "Node.h"
+
+Node::Node(int port, const std::string &ip) {
+}
+
+Node::~Node() {
+}
---
+++ tests/foo-test/test.cpp
@@ -0,0 +1,13 @@
+#include <cstdlib>
+#include <iostream>
+#include <vector>
+#include <map>
+#include <type_traits>
+#include <memory>
+
+#include "gtest/gtest.h"
+#include "gmock/gmock.h"
+
+TEST(gdf_foo_sample_TEST, case1) {
+    //TODO
+}
