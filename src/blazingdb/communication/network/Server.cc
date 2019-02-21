58fd7a608d9a208ec1a0e57e2f87a5e77eff484a
gcca <cristhian@blazingdb.com>
2019-02-21 18:00:49 -0500

    Update server with messages

--- src/blazingdb/communication/network/Server.cc
+++ src/blazingdb/communication/network/Server.cc
@@ -1,5 +1,9 @@
 #include "Server.h"
 
+#include <condition_variable>
+#include <deque>
+#include <mutex>
+
 #include <simple-web-server/server_http.hpp>
 
 namespace blazingdb {
@@ -7,11 +11,45 @@ namespace communication {
 namespace network {
 
 namespace {
+using HttpServer = SimpleWeb::Server<SimpleWeb::HTTP>;
+
+class ConcreteFrame : public Frame {
+public:
+  // TODO: Improve unnecessary copy storing shared ptr for request
+  ConcreteFrame(const std::shared_ptr<HttpServer::Request> &request)
+      : request_{request} {}
+
+  const std::string &data() const final {
+    auto jsonDataIt = request_->header.find("json_data");
+
+    // TODO:
+    // if (request_->header.cend() == jsonDataIt()) {
+    // throw something for bad json header
+    // }
+    return jsonDataIt->second;
+  }
+
+  std::streambuf &buffer() /*const*/ { return *request_->content.rdbuf(); }
+
+private:
+  const std::shared_ptr<HttpServer::Request> request_;
+};
+
 class ConcreteServer : public Server {
 public:
-  using HttpServer = SimpleWeb::Server<SimpleWeb::HTTP>;
+  std::shared_ptr<Frame> GetMessage() /*const*/ final {
+    wait();
+    std::shared_ptr<HttpServer::Request> request = getRequestDeque();
+    return std::make_shared<ConcreteFrame>(request);
+  }
 
-  std::shared_ptr<Message> GetMessage() const final { return nullptr; }
+  std::vector<std::shared_ptr<Frame>> GetMessages(int quantity) {
+    std::vector<std::shared_ptr<Frame>> vector;
+    for (int k = 0; k < quantity; ++k) {
+      vector.emplace_back(GetMessage());
+    }
+    return vector;
+  }
 
   void Run() final {
     httpServer_.config.port = 8000;
@@ -28,13 +66,59 @@ public:
                     << content;
         };
 
+    httpServer_.resource["^/message$"]["POST"] =
+        [this](std::shared_ptr<HttpServer::Response> response,
+               std::shared_ptr<HttpServer::Request> request) {
+          putRequest(request);
+
+          *response << "HTTP/1.1 200 OK\r\nContent-Length: 0\r\n\r\n";
+        };
+
     httpServer_.start();
   }
 
   void Close() noexcept final { httpServer_.stop(); }
 
 private:
+  std::shared_ptr<HttpServer::Request> getRequestDeque() {
+    std::unique_lock<std::mutex> locl(requests_mutex_);
+    std::shared_ptr<HttpServer::Request> request = requests_.back();
+    requests_.pop_back();
+    return request;
+  }
+
+  void wait() {
+    std::unique_lock<std::mutex> lock(condition_mutex_);
+    while (!ready) {
+      condition_variable_.wait(lock);
+    }
+    ready--;
+  }
+
+  void putRequest(const std::shared_ptr<HttpServer::Request> &request) {
+    putRequestsDeque(request);
+    notify();
+  }
+
+  void notify() {
+    std::unique_lock<std::mutex> locl(condition_mutex_);
+    ready++;
+    condition_variable_.notify_one();
+  }
+
+  void putRequestsDeque(const std::shared_ptr<HttpServer::Request> &request) {
+    std::unique_lock<std::mutex> lock(requests_mutex_);
+    requests_.push_front(request);
+  }
+
   HttpServer httpServer_;
+
+  std::mutex requests_mutex_;
+  std::deque<std::shared_ptr<HttpServer::Request>> requests_;
+
+  int ready{0};
+  std::mutex condition_mutex_;
+  std::condition_variable condition_variable_;
 };
 }  // namespace
 
--- src/blazingdb/communication/network/Server.h
+++ src/blazingdb/communication/network/Server.h
@@ -2,6 +2,7 @@
 #define BLAZINGDB_COMMUNICATION_NETWORK_SERVER_H_
 
 #include <memory>
+#include <sstream>
 
 #include <blazingdb/communication/Message.h>
 
@@ -9,11 +10,23 @@ namespace blazingdb {
 namespace communication {
 namespace network {
 
+class Frame {
+public:
+  virtual const std::string &data() const = 0;
+  virtual std::streambuf &buffer() /*const*/ = 0;
+
+  const std::string BufferString() /*const*/ {
+    std::stringstream ss;
+    ss << &buffer();
+    return ss.str();
+  }
+};
+
 class Server {
 public:
   Server() = default;
 
-  virtual std::shared_ptr<Message> GetMessage() const = 0;
+  virtual std::shared_ptr<Frame> GetMessage() /*const*/ = 0;
 
   virtual void Run() = 0;
   virtual void Close() noexcept = 0;
