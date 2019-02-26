77e08b0177f46fea36caa66df6df2bb887aa1968
Christian Noboa Mardini <christian@blazingdb.com>
2019-02-25 23:15:15 -0500

    Added the tests for the messages.

--- tests/component-messages-test/component-messages-test.cpp
+++ tests/component-messages-test/component-messages-test.cpp
@@ -7,11 +7,11 @@
 #include "tests/utils/gdf_column.h"
 #include "tests/utils/gdf_column_cpp.h"
 
-struct QueryMessagesTest : public testing::Test {
-    QueryMessagesTest() {
+struct ComponentMessagesTest : public testing::Test {
+    ComponentMessagesTest() {
     }
 
-    ~QueryMessagesTest() {
+    ~ComponentMessagesTest() {
     }
 
     void SetUp() override {
@@ -21,60 +21,93 @@ struct QueryMessagesTest : public testing::Test {
     }
 };
 
-/*
+
 struct GpuFunctions {
-    static void CopyGpuToCpu(std::string& result, blazingdb::test::gdf_column_cpp& column) {
-        result.append(column.get_gdf_column()->data, column.get_gdf_column()->size);
-        result.append((char*)column.get_gdf_column()->valid, column.get_gdf_column()->size);
+    using DType = blazingdb::test::gdf_dtype;
+    using DTypeInfo = blazingdb::test::gdf_dtype_extra_info;
+    using TimeUnit = blazingdb::test::gdf_time_unit;
+
+    using DataTypePointer = void*;
+    using ValidTypePointer = blazingdb::test::gdf_valid_type*;
+
+
+    static void copyGpuToCpu(std::size_t& binary_pointer, std::string& result, blazingdb::test::gdf_column_cpp& column) {
+        std::size_t data_size = getDataCapacity(column.get_gdf_column());
+        std::memcpy(&result[binary_pointer], column.get_gdf_column()->data, data_size);
+        binary_pointer += data_size;
+
+        std::size_t valid_size = getValidCapacity(column.get_gdf_column());
+        std::memcpy(&result[binary_pointer], column.get_gdf_column()->valid, valid_size);
+        binary_pointer += valid_size;
+    }
+
+    static std::size_t getDataCapacity(blazingdb::test::gdf_column* column) {
+        return column->size;
+    }
+
+    static std::size_t getValidCapacity(blazingdb::test::gdf_column* column) {
+        return column->size * getDTypeSize(column->dtype);
+    }
+
+    static std::size_t getDTypeSize(blazingdb::test::gdf_dtype type) {
+        return blazingdb::test::get_dtype_width(type);
     }
 };
 
 
-TEST_F(QueryMessagesTest, DataScatterMessage) {
-    auto gdf_column_1 = blazingdb::test::build("data sample 1",
-                                               "vali sample 1",
-                                               blazingdb::test::GDF_INT64,
-                                               12,
+TEST_F(ComponentMessagesTest, DataScatterMessage) {
+    auto gdf_column_1 = blazingdb::test::build(8,
+                                               blazingdb::test::GDF_INT16,
+                                               4,
                                                blazingdb::test::TIME_UNIT_ms,
-                                               "column name sample 1");
+                                               "column name 1");
 
-    auto gdf_column_2 = blazingdb::test::build("data sample 2",
-                                               "vali sample 2",
+    auto gdf_column_2 = blazingdb::test::build(16,
                                                blazingdb::test::GDF_INT32,
-                                               35,
+                                               8,
                                                blazingdb::test::TIME_UNIT_s,
-                                               "column name sample 2");
+                                               "column name 2");
 
     auto gdf_column_cpp_1 = blazingdb::test::build(gdf_column_1.get(),
                                                    gdf_column_1->size,
-                                                   gdf_column_1->size,
-                                                   "column name another 1",
-                                                   true,
+                                                   gdf_column_1->size * blazingdb::test::get_dtype_width(gdf_column_1->dtype),
+                                                   "column another name 1",
+                                                   false,
                                                    123);
 
     auto gdf_column_cpp_2 = blazingdb::test::build(gdf_column_2.get(),
                                                    gdf_column_2->size,
-                                                   gdf_column_2->size,
+                                                   gdf_column_2->size * blazingdb::test::get_dtype_width(gdf_column_2->dtype),
                                                    "column name another 2",
                                                    false,
                                                    3567);
-
 
     std::vector<blazingdb::test::gdf_column_cpp> columns;
     columns.emplace_back(gdf_column_cpp_1);
     columns.emplace_back(gdf_column_cpp_2);
 
-    using DataScatterMessage = blazingdb::communication::messages::DataScatterMessage<blazingdb::test::gdf_column_cpp, GpuFunctions>;
+    using DataScatterMessage = blazingdb::communication::messages::DataScatterMessage<blazingdb::test::gdf_column_cpp,
+                                                                                      blazingdb::test::gdf_column,
+                                                                                      GpuFunctions>;
 
-    DataScatterMessage message(columns);
+    std::string json_data;
+    std::string binary_data;
+    {
+        DataScatterMessage message(columns);
 
-    auto ser = message.serializeToJson();
-    std::cout << ser << std::endl;
+        json_data = message.serializeToJson();
+        binary_data = message.serializeToBinary();
+    }
+    {
+        auto message = DataScatterMessage::make(json_data, binary_data);
+        const auto& columns = message->getColumns();
 
-    auto s = message.serializeToBinary();
-    std::cout << "|" << s << "|" << std::endl;
+        ASSERT_TRUE(columns[0] == gdf_column_cpp_1);
+        ASSERT_TRUE(columns[1] == gdf_column_cpp_2);
+    }
 }
 
+/*
 TEST_F(QueryMessagesTest, SampleToNodeMasterMessage) {
     auto gdf_column_1 = blazingdb::test::build("data sample 1",
                                                "vali sample 1",
@@ -125,7 +158,7 @@ TEST_F(QueryMessagesTest, SampleToNodeMasterMessage) {
 }
 */
 
-TEST_F(QueryMessagesTest, PartitionPivotsMessage) {
+TEST_F(ComponentMessagesTest, PartitionPivotsMessage) {
     using Address = blazingdb::communication::Address;
     using NodeToken = blazingdb::communication::NodeToken;
 
