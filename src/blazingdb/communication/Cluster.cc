731cecfe3860a407f8b6a338603d2c84a19b2607
jeanp413 <jeanp413@hotmail.com>
2019-03-04 20:34:35 -0500

    Added missing mutex lock to getAvailableNodes

--- src/blazingdb/communication/Cluster.cc
+++ src/blazingdb/communication/Cluster.cc
@@ -1,16 +1,28 @@
 #include "Cluster.h"
+#include <blazingdb/communication/Address-Internal.h>
 #include <algorithm>
+#include <iostream>
 
 using namespace blazingdb::communication;
 
 void Cluster::addNode(const Node& node) {
   std::unique_lock<std::mutex> lock(condition_mutex);
   nodes_.push_back(std::shared_ptr<Node>(new Node(node)));
+
+  // TODO: Delete this
+  const internal::ConcreteAddress& concreteAddress =
+      *static_cast<const internal::ConcreteAddress*>(node.address());
+
+  const std::string nodeAsString =
+      concreteAddress.ip() + "," + std::to_string(concreteAddress.port());
+  std::cout << nodeAsString << "\n";
 }
 
 size_t Cluster::getTotalNodes() const { return nodes_.size(); }
 
-std::vector<std::shared_ptr<Node>> Cluster::getAvailableNodes(int clusterSize) const {
+std::vector<std::shared_ptr<Node>> Cluster::getAvailableNodes(int clusterSize) {
+  std::unique_lock<std::mutex> lock(condition_mutex);
+
   std::vector<std::shared_ptr<Node>> availableNodes;
   auto copyIter = std::back_inserter(availableNodes);
   auto iterFirst = nodes_.begin();
--- src/blazingdb/communication/Cluster.h
+++ src/blazingdb/communication/Cluster.h
@@ -14,7 +14,7 @@ public:
   explicit Cluster() = default;
   void addNode(const Node& node);
   size_t getTotalNodes() const;
-  std::vector<std::shared_ptr<Node>> getAvailableNodes(int clusterSize) const;
+  std::vector<std::shared_ptr<Node>> getAvailableNodes(int clusterSize);
 
 private:
   std::vector<std::shared_ptr<Node>> nodes_;
