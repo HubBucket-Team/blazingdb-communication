df2c8b1048fecb130db7db1763839ceb2a5b34fe
gcca <cristhian@blazingdb.com>
2019-05-13 13:03:54 -0500

    [feature/ucx] Add configuration for GDR

---
+++ src/blazingdb/communication/Configuration.h
@@ -0,0 +1,25 @@
+#ifndef BLAZINGDB_COMMUNICATION_CONFIGURATION_H_
+#define BLAZINGDB_COMMUNICATION_CONFIGURATION_H_
+
+namespace blazgindb {
+namespace communication {
+
+class Configuration {
+public:
+  static const Configuration&
+  Instance() noexcept;
+
+  static void
+  Set(bool withGDR = false) noexcept;
+
+  virtual bool
+  WithGDR() const noexcept = 0;
+
+protected:
+  explicit Configuration() = default;
+};
+
+}  // namespace communication
+}  // namespace blazgindb
+
+#endif
