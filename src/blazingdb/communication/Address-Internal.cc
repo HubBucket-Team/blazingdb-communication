35b1ff714a5317dd0024341f476339e442e0fa10
Percy Camilo Triveño Aucahuasi <percy@blazingdb.com>
2019-06-20 14:57:44 -0500

    more fixes

--- src/blazingdb/communication/Address-Internal.cc
+++ src/blazingdb/communication/Address-Internal.cc
@@ -4,22 +4,6 @@ namespace blazingdb {
 namespace communication {
 namespace internal {
 
-bool
-ConcreteAddress::SameValueAs(const Address &address) const {
-  const ConcreteAddress &concreteAddress =
-      *static_cast<const ConcreteAddress *>(&address);
-  return (ip_ == concreteAddress.ip_) && (port_ == concreteAddress.port_);
-}
-
-void
-ConcreteAddress::serializeToJson(JsonSerializable::Writer &writer) const {
-  writer.Key("addressIp");
-  writer.String(ip_.c_str());
-
-  writer.Key("addressPort");
-  writer.Int(port_);
-};
-
 }  // namespace internal
 }  // namespace communication
 }  // namespace blazingdb
