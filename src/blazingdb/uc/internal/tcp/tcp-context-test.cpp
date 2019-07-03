8a8f843ad803906c6cab447bee46b12fff751a21
Percy Camilo Triveño Aucahuasi <percy@blazingdb.com>
2019-07-03 14:27:58 -0500

    add basic tcp test

--- src/blazingdb/uc/internal/tcp/tcp-context-test.cpp
+++ src/blazingdb/uc/internal/tcp/tcp-context-test.cpp
@@ -4,89 +4,49 @@
 
 #include "../../api-common-test.hpp"
 
-void
-Sender(int (&pipedes)[2],
-       const pid_t       pid,
-       const std::size_t incrementalLength) {
-  cuInit(0);
-  close(pipedes[0]);
-
-  const void *data = CreateData(incrementalLength, ownSeed, 0);
-
-  auto context = Context::IPC();
-  auto agent   = context->Agent();
-  auto buffer  = agent->Register(data, incrementalLength);
-
-  auto serializedRecord = buffer->SerializedRecord();
-
-  write(pipedes[1], serializedRecord->Data(), serializedRecord->Size());
-
-  int stat_loc;
-  waitpid(pid, &stat_loc, WUNTRACED | WCONTINUED);
-  EXPECT_EQ(0, stat_loc);
-  close(pipedes[1]);
-  std::exit(EXIT_SUCCESS);
-}
-
-void
-Receiver(int (&pipedes)[2], const std::size_t incrementalLength) {
-  cuInit(0);
-  close(pipedes[1]);
-
-  const void *data = CreateData(incrementalLength, peerSeed, 0);
-
-  auto context = Context::IPC();
-  auto agent   = context->Agent();
-  auto buffer  = agent->Register(data, incrementalLength);
-
-  std::uint8_t *recordData = new std::uint8_t[context->serializedRecordSize()];
-  read(pipedes[0], recordData, context->serializedRecordSize());
-  auto transport = buffer->Link(recordData);
-
-  auto future = transport->Get();
-  future.wait();
-
-  const void *const expected = CreateHostData(incrementalLength, ownSeed, 0);
-
-  std::uint8_t *result = new std::uint8_t[incrementalLength];
-
-  cudaError_t cudaError;
-
-  cudaError =
-      cudaMemcpy(result, data, incrementalLength, cudaMemcpyDeviceToHost);
-  EXPECT_EQ(cudaSuccess, cudaError);
-
-  cudaError = cudaDeviceSynchronize();
-  EXPECT_EQ(cudaSuccess, cudaError);
-
-  EXPECT_EQ(0, std::memcmp(expected, result, incrementalLength));
-
-  delete[] recordData;
-
-  close(pipedes[0]);
-  std::exit(EXIT_SUCCESS);
-}
-
-TEST(TCPContextTest, Sending) {
-  using blazingdb::uc::Context;
+TEST(TCPOnProcessesTest, Direct) {
+  using namespace blazingdb::uc;
 
   int pipedes[2];
   pipe(pipedes);
 
   pid_t pid = fork();
+
   ASSERT_NE(-1, pid);
 
+  static constexpr std::size_t length = 20;
+
   if (pid) {
-    pid = fork();
-    ASSERT_NE(-1, pid);
-    if (pid) {
-      int stat_loc;
-      waitpid(pid, &stat_loc, WCONTINUED);
-      EXPECT_EQ(0, stat_loc);
-    } else {
-      Sender(pipedes, pid, 1000);
-    }
+    close(pipedes[0]);
+    const void *data = CreateData(length, ownSeed, ownOffset);
+    Print("own", data, length);
+
+    auto context = Context::IPC();
+    auto agent   = context->Agent();
+    auto buffer  = agent->Register(data, length);
+
+    auto serializedRecord = buffer->SerializedRecord();
+
+    write(pipedes[1], serializedRecord->Data(), serializedRecord->Size());
+    int stat_loc;
+    waitpid(pid, &stat_loc, WUNTRACED | WCONTINUED);
   } else {
-    Receiver(pipedes, 1000);
+    close(pipedes[1]);
+    const void *data = CreateData(length, peerSeed, peerOffset);
+    Print("peer", data, length);
+
+    auto context = Context::IPC();
+    auto agent   = context->Agent();
+    auto buffer  = agent->Register(data, length);
+
+    std::uint8_t recordData[104];
+    read(pipedes[0], recordData, 104);
+    auto transport = buffer->Link(recordData);
+
+    auto future = transport->Get();
+    future.wait();
+
+    Print("peer", data, length);
+    std::exit(EXIT_SUCCESS);
   }
 }
